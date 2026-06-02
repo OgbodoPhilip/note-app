87fbe88688d02726e2b46b6d8ece4c88f5002cda
Ogbodophilip <mrogbodophilip@gmail.com>
2026-06-02 00:43:08 -0700

    rate limiting added to the project to prevent multiple request in a given time

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/rateLimiter.js';
+
 import connectDB from './config/db.js';
 dotenv.config();
+import noteRoutes from "./routes/noteRoutes.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,7 +15,15 @@ await connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(rateLimiter);
 
+app.get('/',(req,res)=>{
+    res.status(200).send("THE NOTE APPLICATION")
+})
+
+
+app.use('/api/notes',noteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
